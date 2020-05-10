d9e15fbacab18e25b0d40c300a4cc3d6dfdd729c
Rajendraverma1989 <rajendraverma1234@gmail.com>
2020-05-10 14:50:29 +0530

    Updates

---
+++ precache-manifest.222a2d4ceb3e6a39846649d541460e7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc66e56f6abf2f0fd42b3456ff18eaf",
+    "url": "/Rajendraverma1989/monsters-rolodex.git/index.html"
+  },
+  {
+    "revision": "56dbb80f0e2017d55004",
+    "url": "/Rajendraverma1989/monsters-rolodex.git/static/css/main.6db54891.chunk.css"
+  },
+  {
+    "revision": "caaf729858c947b16a2f",
+    "url": "/Rajendraverma1989/monsters-rolodex.git/static/js/2.ee096883.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Rajendraverma1989/monsters-rolodex.git/static/js/2.ee096883.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56dbb80f0e2017d55004",
+    "url": "/Rajendraverma1989/monsters-rolodex.git/static/js/main.9aba03c7.chunk.js"
+  },
+  {
+    "revision": "98a24633438ae5e4482d",
+    "url": "/Rajendraverma1989/monsters-rolodex.git/static/js/runtime-main.abe8bd19.js"
+  }
+]);
